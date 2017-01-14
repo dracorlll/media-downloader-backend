05a93d09c3bbffa57f0a34960e1b63aa772e28a6
Engin Yüksel <info@enginyuksel.kim>
2017-01-14 23:14:02 +0300

    başka video kaynakları eklendi

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -9,7 +9,6 @@ import re
 
 class YoutubeView(APIView):
 
-
     permission_classes = (permissions.AllowAny,)
     authentication_classes = (SessionAuthentication, TokenAuthentication,)
     parser_classes = (JSONParser,)
@@ -19,22 +18,114 @@ class YoutubeView(APIView):
 
     def post(self, request, format=None):
         url = request.data.get('url', None)
-        regex = r'^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$'
-        # quality = request.data.get('quality', None)
-        if re.match(regex, str(url)):
-            shared_url = share_url(url)
-            return Response({"success": True,
-                             "url": shared_url
+        regex_yt = r'^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$'
+        regex_vk = r'^((?:https?:)?\/\/)?((?:www)\.)?((?:vk.com))'
+        regex_fb = r'^((https|http):\/\/(www|m).facebook.com)'
+        regex_dm = r'^((http|https):\/\/(www|m).dailymotion.com)'
+        regex_ru = r'(?:^|\W)mail.ru(?:$|\W)'
+        if re.match(regex_yt, str(url)):
+            title, video = yt_url(url)
+            return Response({
+                             "data": video,
+                             "title": title,
+                             "success": True,
                              })
+        elif re.match(regex_vk, str(url)):
+            title, video = vk_url(url)
+            return Response({
+                "data": video,
+                "title": title,
+                "success": True,
+            })
+        # elif re.match(regex_fb, str(url)):
+        #     title, video = fb_url(url)
+        #     return Response({
+        #         "data": video,
+        #         "title": title,
+        #         "success": True,
+        #     })
+        elif re.match(regex_dm, str(url)):
+            title, video = dm_url(url)
+            return Response({
+                "data": video,
+                "title": title,
+                "success": True,
+            })
+        elif re.search(regex_ru, str(url)):
+            title, video = ru_url(url)
+            return Response({
+                "data": video,
+                "title": title,
+                "success": True,
+            })
         else:
-            return Response({"success": False})
+            # return Response({"success": False})
+            ydl = YoutubeDL()
+            r = ydl.extract_info(url, download=False)
+            return Response({
+                "data": r
+            })
 
 
-def share_url(self):
-    url = ""
+def yt_url(self):
     ydl = YoutubeDL()
+    video = {}
     r = ydl.extract_info(self, download=False)
+    title = r['title']
     for formats in r['formats']:
-        if formats['format_id'] == '18':
+        if (formats['format_id'] == '18') or (formats['format_id'] == '140') or (formats['format_id'] == '22'):
             url = formats['url']
-    return url
+            ext = formats['ext']
+            liste = {'url': url, 'ext': ext}
+            video[formats['format_id']] = liste
+    return title, video
+
+
+def vk_url(self):
+    url = re.split(r'((?:video-?)[0-9]+_[0-9]+)', str(self))
+    video = {}
+    title = url[1]
+    video[0] = {'url': "https://vk.com/" + url[1], 'ext': 'mp4'}
+    return title, video
+
+
+def fb_url(self):
+    ydl = YoutubeDL()
+    video = {}
+    r = ydl.extract_info(self, download=False)
+    title = r['title']
+    for formats in r['formats']:
+        if formats['format_id'] == 'dash_hd_src_no_ratelimit':
+            url = formats['url']
+            ext = formats['ext']
+            liste = {'url': url, 'ext': ext}
+            video[formats['format_id']] = liste
+    return title, video
+
+
+def dm_url(self):
+    ydl = YoutubeDL()
+    video = {}
+    r = ydl.extract_info(self, download=False)
+    title = r['title']
+    for formats in r['formats']:
+        if (formats['format_id'] == 'http-380') or (formats['format_id'] == 'http-480') or (formats['format_id'] == 'http-240') or (formats['format_id'] == 'http-720') or (formats['format_id'] == 'http-1080') or (formats['format_id'] == 'http-360'):
+            url = formats['url']
+            ext = formats['ext']
+            liste = {'url': url, 'ext': ext}
+            video[formats['format_id']] = liste
+    return title, video
+
+
+def ru_url(self):
+    ydl = YoutubeDL()
+    video = {}
+    r = ydl.extract_info(self, download=False)
+    title = r['title']
+    for formats in r['formats']:
+        if (formats['format_id'] == '360p') or (formats['format_id'] == '240p') or (formats['format_id'] == '480p') or (formats['format_id'] == '720p') or (formats['format_id'] == '1080p'):
+            url = formats['url']
+            ext = formats['ext']
+            liste = {'url': url, 'ext': ext}
+            video[formats['format_id']] = liste
+    return title, video
