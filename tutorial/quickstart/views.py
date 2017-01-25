58814293f6b44cbf115a9dad59b8459223a35e1e
Engin Yüksel <info@enginyuksel.kim>
2017-01-25 10:14:32 +0300

    [EKLEME]thumbnail ve error mesajı

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -20,62 +20,70 @@ class YoutubeView(APIView):
     def post(self, request, format=None):
         url = request.data.get('url', None)
         if re.match(regex_yt, str(url)):
-            title, video = yt_url(url)
+            thumbnail, title, video = yt_url(url)
             return Response({
                              "data": video,
                              "title": title,
+                             "thumbnail": thumbnail,
                              "success": True,
                              })
         elif re.match(regex_vk, str(url)):
-            title, video = vk_url(url)
+            thumbnail, title, video = vk_url(url)
             return Response({
                 "data": video,
                 "title": title,
+                "thumbnail": thumbnail,
                 "success": True,
             })
         elif re.match(regex_fb, str(url)):
-            title, video = fb_url(url)
+            thumbnail, title, video = fb_url(url)
             return Response({
                 "data": video,
                 "title": title,
+                "thumbnail": thumbnail,
                 "success": True,
             })
         elif re.match(regex_dm, str(url)):
-            title, video = dm_url(url)
+            thumbnail, title, video = dm_url(url)
             return Response({
                 "data": video,
                 "title": title,
+                "thumbnail": thumbnail,
                 "success": True,
             })
         elif re.search(regex_ru, str(url)):
-            title, video = ru_url(url)
+            thumbnail, title, video = ru_url(url)
             return Response({
                 "data": video,
                 "title": title,
+                "thumbnail": thumbnail,
                 "success": True,
             })
         elif re.search(regex_vimeo, str(url)):
-            title, video = vimeo_url(url)
+            thumbnail, title, video = vimeo_url(url)
             return Response({
                 "data": video,
                 "title": title,
+                "thumbnail": thumbnail,
                 "success": True,
             })
         elif re.search(regex_url, str(url)):
             ydl = YoutubeDL()
-            info_dict = ydl.extract_info(url, download=False)
-            r = info_dict.get('format', 'best')
-            if r.__len__() == 0:
+            try:
+                info_dict = ydl.extract_info(url, download=False)
+            except Exception as e:
                 return Response({
+                    'error': e.message,
                     'success': False
                 })
-            else:
-                title, video = other_url(info_dict)
-                return Response({
-                    "data": video,
-                    "title": title,
-                    "success": True
-                })
+            thumbnail, title, video = other_url(info_dict)
+            return Response({
+                "data": video,
+                "title": title,
+                "thumbnail": thumbnail,
+                "success": True
+            })
+
         else:
             return Response({
                 'success': False
@@ -86,8 +94,20 @@ def other_url(self):
     format_id = str(self.get('format', 'best')).split(' ').__getitem__(0)
     video = {}
     title = self['title']
-    for formats in self['formats']:
-        if formats['format_id'] == format_id:
+    thumbnail = self['thumbnails'][0]['url']
+    if not format_id:
+        for formats in self['formats']:
+            if formats['format_id'] == format_id:
+                url = formats['url']
+                ext = formats['ext']
+                try:
+                    filesize = formats['filesize']
+                except KeyError:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize}
+                video['best'] = liste
+    else:
+        for formats in self['formats']:
             url = formats['url']
             ext = formats['ext']
             try:
@@ -96,7 +116,7 @@ def other_url(self):
                 filesize = None
             liste = {'url': url, 'ext': ext, 'size': filesize}
             video['best'] = liste
-    return title, video
+    return thumbnail, title, video
 
 
 def vimeo_url(self):
@@ -105,6 +125,7 @@ def vimeo_url(self):
     video = {}
     r = ydl.extract_info(self, download=False)
     title = r['title']
+    thumbnail = r['thumbnails'][0]['url']
     for formats in r['formats']:
         if re.search(regex, formats['format_id']):
             url = formats['url']
@@ -115,7 +136,7 @@ def vimeo_url(self):
                 filesize = None
             liste = {'url': url, 'ext': ext, 'size': filesize}
             video[str(formats['format_id']).split('-').__getitem__(1)] = liste
-    return title, video
+    return thumbnail, title, video
 
 
 def yt_url(self):
@@ -123,6 +144,7 @@ def yt_url(self):
     video = {}
     r = ydl.extract_info(self, download=False)
     title = r['title']
+    thumbnail = r['thumbnails'][0]['url']
     for formats in r['formats']:
         if (formats['format_id'] == '18') or (formats['format_id'] == '140') or (formats['format_id'] == '22'):
             url = formats['url']
@@ -136,7 +158,7 @@ def yt_url(self):
                 video[str(formats['height']) + 'p'] = liste
             else:
                 video['audio'] = liste
-    return title, video
+    return thumbnail, title, video
 
 
 def vk_url(self):
@@ -145,6 +167,7 @@ def vk_url(self):
     video = {}
     r = ydl.extract_info(self, download=False)
     title = r['title']
+    thumbnail = r['thumbnails'][0]['url']
     for formats in r['formats']:
         if re.match(regex, formats['format_id']) and formats['format_id'] != 'hls-meta':
             url = formats['url']
@@ -155,7 +178,7 @@ def vk_url(self):
                 filesize = None
             liste = {'url': url, 'ext': ext, 'size': filesize}
             video[str(formats['format']).split('x').__getitem__(1) + 'p'] = liste
-    return title, video
+    return thumbnail, title, video
 
 
 def fb_url(self):
@@ -163,6 +186,7 @@ def fb_url(self):
     video = {}
     r = ydl.extract_info(self, download=False)
     title = r['title']
+    thumbnail = r['thumbnails'][0]['url']
     for formats in r['formats']:
         if formats['format_id'] == 'dash_hd_src_no_ratelimit':
             url = formats['url']
@@ -182,7 +206,7 @@ def fb_url(self):
                 filesize = None
             liste = {'url': url, 'ext': ext, 'size': filesize}
             video['standart quality'] = liste
-    return title, video
+    return thumbnail, title, video
 
 
 def dm_url(self):
@@ -190,6 +214,7 @@ def dm_url(self):
     video = {}
     r = ydl.extract_info(self, download=False)
     title = r['title']
+    thumbnail = r['thumbnails'][0]['url']
     for formats in r['formats']:
         if (formats['format_id'] == 'http-380') or (formats['format_id'] == 'http-480') or (formats['format_id'] == 'http-240') or (formats['format_id'] == 'http-720') or (formats['format_id'] == 'http-1080') or (formats['format_id'] == 'http-360'):
             url = formats['url']
@@ -200,7 +225,7 @@ def dm_url(self):
                 filesize = None
             liste = {'url': url, 'ext': ext, 'size': filesize}
             video[str(formats['format_id']).split('-').__getitem__(1) + 'p'] = liste
-    return title, video
+    return thumbnail, title, video
 
 
 def ru_url(self):
@@ -208,6 +233,7 @@ def ru_url(self):
     video = {}
     r = ydl.extract_info(self, download=False)
     title = r['title']
+    thumbnail = r['thumbnails'][0]['url']
     for formats in r['formats']:
         if (formats['format_id'] == '360p') or (formats['format_id'] == '240p') or (formats['format_id'] == '480p') or (formats['format_id'] == '720p') or (formats['format_id'] == '1080p'):
             url = formats['url']
@@ -218,4 +244,4 @@ def ru_url(self):
                 filesize = None
             liste = {'url': url, 'ext': ext, 'size': filesize}
             video[formats['format_id']] = liste
-    return title, video
+    return thumbnail, title, video
