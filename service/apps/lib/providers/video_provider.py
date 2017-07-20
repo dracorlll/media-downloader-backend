38bac126d2507f714ecfaafda65ff850c2b8fb4f
Engin Yüksel <info@enginyuksel.kim>
2017-07-20 19:47:26 +0300

    added some other web sources

--- service/apps/lib/providers/video_provider.py
+++ service/apps/lib/providers/video_provider.py
@@ -31,9 +31,11 @@ import re
 
 from django.conf import settings
 from youtube_dl import YoutubeDL
+import json
 
 
 class VideoProvider:
+
     ydl = None
 
     def __init__(self, proxy):
@@ -45,6 +47,7 @@ class VideoProvider:
         video = []
         r = self.ydl.extract_info(url, download=False)
         title = r['title']
+        print self.ydl.list_formats(r)
         thumbnail = r['thumbnails'][0]['url']
         for formats in r['formats']:
             if re.search(regex, formats['format_id']):
@@ -83,7 +86,7 @@ class VideoProvider:
         return thumbnail, title, video
 
     def vk(self, url):
-        regex = r'(hls-)'
+        regex = r'(cache)'
         video = []
         r = self.ydl.extract_info(url, download=False)
         title = r['title']
@@ -92,7 +95,27 @@ class VideoProvider:
             if re.match(regex, formats['format_id']):
                 url = formats['url']
                 ext = formats['ext']
-                format_id = str(formats['format']).split('url').__getitem__(1) + 'p'
+                format_id = formats['format_id']
+                print format_id
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id}
+                video.append(liste)
+        return thumbnail, title, video
+
+    def ins(self, url):
+        regex = r'(0)'
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if re.match(regex, formats['format_id']):
+                url = formats['url']
+                ext = formats['ext']
+                format_id = formats['format_id']
                 print format_id
                 try:
                     filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
@@ -106,7 +129,7 @@ class VideoProvider:
         video = []
         r = self.ydl.extract_info(url, download=False)
         title = r['title']
-        thumbnail = ''
+        thumbnail = r['thumbnails'][0]['url']
         for formats in r['formats']:
             if formats['format_id'] == 'dash_hd_src_no_ratelimit':
                 url = formats['url']
@@ -171,6 +194,214 @@ class VideoProvider:
                 video.append(liste)
         return thumbnail, title, video
 
+    def yahoo(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if str(formats['format_id']).split('-').__getitem__(0) == 'mp4':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = str(formats['height']) + 'p'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste)
+        return thumbnail, title, video
+
+    def breakvideo(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if str(formats['format_id']).split('-').__getitem__(0) == 'http':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = str(formats['height']) + 'p'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste)
+        return thumbnail, title, video
+
+    def flicker(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        try:
+            thumbnail = r['thumbnails'][0]['url']
+        except:
+            thumbnail = None
+
+        for formats in r['formats']:
+            if formats['format_id'] == '700':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = 'standart'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste)
+        return thumbnail, title, video
+
+    def funny(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if str(formats['format_id']).split('-').__getitem__(0) == 'mp4':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = str(formats['height']) + 'p'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste)
+        return thumbnail, title, video
+
+    def liveleak(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if formats['format_id'] == '0' or formats['format_id'] == '1':
+                url = formats['url']
+                ext = formats['ext']
+                if formats['height'] == None:
+                    format_id = '240p'
+                else:
+                    format_id = str(formats['height']) + 'p'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste)
+        return thumbnail, title, video
+
+    def ted(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if str(formats['format_id']).split('-').__getitem__(0) == 'http':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = str(formats['height']) + 'p'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste),
+        return thumbnail, title, video
+
+    def twitch(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if formats['format_id'] == 'Audio_Only' or 'Source':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = formats['format_id']
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste),
+            else:
+                url = formats['url']
+                ext = formats['ext']
+                format_id = formats['format_id']
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste),
+        return thumbnail, title, video
+
+    def twitter(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if str(formats['format_id']).split('-').__getitem__(0) == 'hls':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = str(formats['height']) + 'p'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste),
+        return thumbnail, title, video
+
+    def ustream(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            url = formats['url']
+            ext = formats['ext']
+            format_id = 'standart'
+            try:
+                filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+            except:
+                filesize = None
+            liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                     'http_headers': formats['http_headers']}
+            video.append(liste),
+
+        return thumbnail, title, video
+
+    def viewster(self, url):
+        video = []
+        r = self.ydl.extract_info(url, download=False)
+        title = r['title']
+        thumbnail = r['thumbnails'][0]['url']
+        for formats in r['formats']:
+            if str(formats['format_id']).split('-').__getitem__(0) == 'hls':
+                url = formats['url']
+                ext = formats['ext']
+                format_id = str(formats['height']) + 'p'
+                try:
+                    filesize = str(float("{0:.2f}".format(float(formats['filesize']) / 1048576))) + ' Mb'
+                except:
+                    filesize = None
+                liste = {'url': url, 'ext': ext, 'size': filesize, 'format': format_id,
+                         'http_headers': formats['http_headers']}
+                video.append(liste),
+        return thumbnail, title, video
+
     def get_video_provider(self, url):
         func = None
         if re.match(settings.REGEX_YT, str(url)):
@@ -185,4 +416,26 @@ class VideoProvider:
             func = self.ru
         elif re.search(settings.REGEX_VIMEO, str(url)):
             func = self.vimeo
+        elif re.search(settings.REGEX_INS, str(url)):
+            func = self.ins
+        elif re.search(settings.REGEX_YAHOO, str(url)):
+            func = self.yahoo
+        elif re.search(settings.REGEX_BREAK, str(url)):
+            func = self.breakvideo
+        elif re.search(settings.REGEX_FLICKER, str(url)):
+            func = self.flicker
+        elif re.search(settings.REGEX_FUNNY, str(url)):
+            func = self.funny
+        elif re.search(settings.REGEX_LIVELEAK, str(url)):
+            func = self.liveleak
+        elif re.search(settings.REGEX_TED, str(url)):
+            func = self.ted
+        elif re.search(settings.REGEX_TWITCH, str(url)):
+            func = self.twitch
+        elif re.search(settings.REGEX_TWITTER, str(url)):
+            func = self.twitter
+        elif re.search(settings.REGEX_USTREAM, str(url)):
+            func = self.ustream
+        elif re.search(settings.REGEX_VIEWSTER, str(url)):
+            func = self.viewster
         return func
